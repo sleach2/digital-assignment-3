8e1f09275883e662bb39ebbf2f97e080d5d4dd07
sleach2 <sleach2@gmu.edu>
2015-02-19 11:38:46 -0500

    qqwer

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ window.onload = function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         land=game.add.tileSprite(0,0,2000,2000,'bricks');
         land.fixedToCamera=true;
-        door=game.add.sprite(1400,600,'door');
+        door=game.add.sprite(1450,650,'door');
         //door.body.immovable=true;
         enemies=game.add.group();
         enemies.enableBody=true;
