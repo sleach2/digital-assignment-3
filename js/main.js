15907a08189e8f705e2bc21337b8d654fb609b4a
sleach2 <sleach2@gmu.edu>
2015-02-19 00:31:01 -0500

    as

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ window.onload = function() {
         land.fixedToCamera=true;
         player = game.add.sprite(0, 0, 'dude2');
         player.anchor.setTo(0.5,0.5);
-        game.physics.arcade.enable(player);
+        game.physics.enable(player,Phaser.Physics.ARCADE);
         player.body.collideWorldBounds = true;
         move = game.input.keyboard.createCursorKeys();
         /*game.camera.follow(player);
