324c031870ac8a954d95da7fa3fc8fa3d12bb157
sleach2 <sleach2@gmu.edu>
2015-02-19 02:48:51 -0500

    gng

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ window.onload = function() {
         game.load.image('dude2', 'assets/dude2.png');
         game.load.image('man','assets/man.png');
         game.load.image('enemy','assets/player.png');
+        game.load.image('door', 'assets/door.png');
     }
     
     var player=null;
@@ -22,16 +23,14 @@ window.onload = function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         land=game.add.tileSprite(0,0,2000,2000,'bricks');
         land.fixedToCamera=true;
+        game.add.sprite(1500,700,'door');
         enemies=game.add.group();
-        //enemies.enableBody=true;
+        enemies.enableBody=true;
         player = game.add.sprite(0, 0, 'dude2');
         player.anchor.setTo(0.5,0.5);
         game.physics.enable(player,Phaser.Physics.ARCADE);
         player.body.collideWorldBounds = true;
         move = game.input.keyboard.createCursorKeys();
-        /*game.camera.follow(player);
-        game.camera.deadzone = new Phaser.Rectangle(100, 100, 800, 600);
-        game.camera.focusOnXY(player.x, player.y);*/
         timer = game.time.create(false);
         timer.loop(2000, spawn, this);
         timer.start();
@@ -43,29 +42,17 @@ window.onload = function() {
     }
     
     function update() {
-        //player.body.setZeroVelocity();
-        player.body.velocity.x=0;
-        player.body.velocity.y=0;
+        player.body.setZeroVelocity();
         if (move.left.isDown){ 
             player.body.velocity.x -= 250; 
         }else if (move.right.isDown){ 
             player.body.velocity.x += 250;
-            //player.body.moveRight(250);
         }    
          if(move.up.isDown){
             player.body.velocity.y -= 250;
-            //player.body.moveUp(250);
         }else if(move.down.isDown){
             player.body.velocity.y += 250;
-            //player.body.moveDown(250);
         }
-        //enemies.forEach(function(enemy) {this.moveToObject(enemy, player, 20);}, game.physics);
+        enemies.forEach(function(enemy) {this.moveToObject(enemy, player, 20);}, game.physics);
     }
-
-    /*function render() {
-
-    game.debug.text('Time until event: ' + timer.duration.toFixed(0), 32, 32);
-    game.debug.text('Loop Count: ' + total, 32, 64);
-
-    }*/
 };
