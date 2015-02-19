f04f0080019bf34e749cbf6ae527052f41b896ab
sleach2 <sleach2@gmu.edu>
2015-02-19 14:04:39 -0500

    tr

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ window.onload = function() {
         land.fixedToCamera=true;
         door=game.add.sprite(1425,625,'door');
         game.physics.enable(door,Phaser.Physics.ARCADE);
-        //door.body.immovable=true;
+        door.body.immovable=true;
         enemies=game.add.group();
         enemies.enableBody=true;
         player = game.add.sprite(0, 0, 'dude2');
