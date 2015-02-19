77cdb2444dc0b6153f531c9b0def440b18f18828
sleach2 <sleach2@gmu.edu>
2015-02-18 20:01:49 -0500

    y

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ window.onload = function() {
     
     function preload() {
         game.load.image( 'bricks', 'assets/bricks.png' );
-        game.load.image('dude', 'assets/dude.png');
+        game.load.image('dude', 'assets/dude.png',32,48);
     }
     
     var player;
@@ -14,12 +14,12 @@ window.onload = function() {
     var land
 
     function create() {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+        //game.physics.startSystem(Phaser.Physics.ARCADE);
         game.world.setBounds(-1000, -1000, 2000, 2000);
         land=game.add.tileSprite(0,0,800,600,'bricks');
         land.fixedToCamera=true;
         player = game.add.sprite(32, game.world.height - 150, 'dude');
-        //game.physics.enable(player, Phaser.Physics.ARCADE)
+        game.physics.enable(player, Phaser.Physics.ARCADE)
         player.body.collideWorldBounds = true;
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
@@ -29,7 +29,6 @@ window.onload = function() {
     
     function update() {
         player.body.velocity.x = 0;
-        player.body.velocity.y=0;
         if (move.left.isDown){ 
              player.body.velocity.x = -250; 
              player.animations.play('left'); 
