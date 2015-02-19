47e80b686b7b8a5112d6fd8ee518ef801fbc2913
sleach2 <sleach2@gmu.edu>
2015-02-19 00:23:43 -0500

    h

--- js/main.js
+++ js/main.js
@@ -2,43 +2,48 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1500, 700, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
         game.load.image( 'bricks', 'assets/bricks.png' );
         game.load.image('dude2', 'assets/dude2.png');
         game.load.image('man','assets/man.png');
-        game.load.image('enemy', 'assets/player.png');
+        game.load.image('enemy','assets/player.png');
     }
     
     var player;
     var move;
     var land;
+    var enemy;
 
     function create() {
         game.physics.startSystem(Phaser.Physics.P2JS);
         land=game.add.tileSprite(0,0,2000,2000,'bricks');
         land.fixedToCamera=true;
-        player = game.add.sprite(0, 0, 'dude2');
+        player = game.add.sprite(0, 0, 'man');
         player.anchor.setTo(0.5,0.5);
         game.physics.p2.enable(player);
         player.body.collideWorldBounds = true;
         move = game.input.keyboard.createCursorKeys();
         game.camera.follow(player);
+        game.camera.deadzone = new Phaser.Rectangle(100, 100, 800, 600);
+        game.camera.focusOnXY(player.x, player.y);
     }
     
     function update() {
-        //player.body.setZeroVelocity();
-        player.body.velocity.x=0;
-        player.body.velocity.y=0;   
+        player.body.setZeroVelocity();
+        
         if (move.left.isDown){ 
             player.body.velocity.x = -250; 
         }else if (move.right.isDown){ 
+            //player.body.velocity.x += 250;
             player.body.moveRight(250);
         }    
          if(move.up.isDown){
+            //player.body.velocity.y -= 250;
             player.body.moveUp(250);
         }else if(move.down.isDown){
+            //player.body.velocity.y += 250;
             player.body.moveDown(250);
         }
     }
