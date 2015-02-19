68c4f2ef337e821cc750139b822d5cdb7c5adf9d
sleach2 <sleach2@gmu.edu>
2015-02-19 03:00:14 -0500

    w

--- js/main.js
+++ js/main.js
@@ -57,13 +57,13 @@ window.onload = function() {
             player.body.velocity.y += 250;
         }
         //enemies.forEach(function(enemy) {this.moveToObject(enemy, player, 20);}, game.physics);
-        if(game.physics.arcade.collide(player, door)){
+        /*if(game.physics.arcade.collide(player, door)){
             game.add.text(160, 150, 'You Win!', { fontSize: '64px', fill: '#000' });
             player.kill();
         }
         if(game.physics.arcade.collide(player, enemies)){
             game.add.text(160, 150, 'You Lose!', { fontSize: '64px', fill: '#000' });
             player.kill();
-        }
+        }*/
     }
 };
