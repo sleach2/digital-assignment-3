ec88258fad9eab11b730146978df48d235e1337e
sleach2 <sleach2@gmu.edu>
2015-02-19 00:57:32 -0500

    u

--- js/main.js
+++ js/main.js
@@ -61,10 +61,10 @@ window.onload = function() {
         }
     }
 
-    function render() {
+    /*function render() {
 
     game.debug.text('Time until event: ' + timer.duration.toFixed(0), 32, 32);
     game.debug.text('Loop Count: ' + total, 32, 64);
 
-}
+    }*/
 };
