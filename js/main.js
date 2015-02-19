73a4713f87aee695238f2d98cfa10011a001f1f8
sleach2 <sleach2@gmu.edu>
2015-02-18 22:19:07 -0500

    g

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ window.onload = function() {
         game.world.setBounds(0, 0, 2000, 2000);
         land=game.add.tileSprite(0,0,2000,2000,'bricks');
         land.fixedToCamera=true;
-        player = game.add.sprite(32, game.world.height - 150, 'man');
+        player = game.add.sprite(32, game.world.height - 150, 'dude2');
         //game.physics.enable(player, Phaser.Physics.ARCADE)
         game.physics.arcade.enable(player);
         player.anchor.setTo(0.5,0.5);
